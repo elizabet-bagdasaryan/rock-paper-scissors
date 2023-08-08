4cadc7c3a129e251bf47df007268d35ec09c80a8
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-08-08 13:09:59 +0400

    feat: add details for game outputs in the console

---
+++ index.js
@@ -0,0 +1,36 @@
+import RockPaperScissors from "./game.js";
+import { createInterface } from "readline";
+
+const moves = process.argv.slice(2);
+
+if (moves.length < 3 || moves.length % 2 === 0 || new Set(moves).size !== moves.length) {
+  console.error('Invalid input! Please provide an odd number >= 3 of non-repeating strings.');
+  console.error('Example: node game.js rock paper scissors');
+  process.exit(1);
+}
+
+const game = new RockPaperScissors(moves);
+game.showTable();
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function askUserMove() {
+    readline.question('Enter your move: ', (userMove) => {
+        if (userMove === '0') {
+          console.log('Exiting the game.');
+          readline.close();
+          process.exit(0);
+        } else if (userMove === '?') {
+          game.showTable();
+          askUserMove();
+        } else {
+          game.playGame(moves[parseInt(userMove) - 1]);
+          askUserMove();
+        }
+      });
+}
+
+askUserMove();
